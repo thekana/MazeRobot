d8045c5333ab994128b12ac3ebf021f706484789
Kanadech Jirapongtanavech <kanadech.king@gmail.com>
2019-07-14 04:24:06 +1000

    WIP

--- PhaseB/newmaze3d/Maze.h
+++ PhaseB/newmaze3d/Maze.h
@@ -2,204 +2,281 @@
 #define MAZE_H
 #include <Arduino.h>
 #include <avr/pgmspace.h>
+
 byte x2i(char *s)
 {
   byte x = 0;
-  for (;;) {
+  for (;;)
+  {
     char c = *s;
-    if (c >= '0' && c <= '9') {
+    if (c >= '0' && c <= '9')
+    {
       x *= 16;
       x += c - '0';
     }
-    else if (c >= 'A' && c <= 'F') {
+    else if (c >= 'A' && c <= 'F')
+    {
       x *= 16;
       x += (c - 'A') + 10;
     }
-    else break;
+    else
+      break;
     s++;
   }
   return x;
 }
-enum heading {
-  EAST = 0,
-  SOUTH = 1,
-  WEST = 2,
-  NORTH = 3
+enum heading
+{
+  EAST = 3,
+  SOUTH = 2,
+  WEST = 1,
+  NORTH = 0
 };
 typedef enum heading Head;
 
-class Maze {
-  private:
-    byte cells[5][9][4];
-    String statusCells[5][9];
-    byte rows = 5;
-    byte cols = 9;
-    byte startX = 0;
-    byte startY = 0;
-    Head head;
-  public:
-    Maze(String const &h) {
-      this->rows = 5;
-      this->cols = 9;
-      initializeCells();  // 2 is unexplored
-      initializeStatusCells(); // 4 spaces
-      statusCells[2][4] = " X ";  // mark goal
+class Maze
+{
+private:
+  byte cells[5][9] = {0};
+  byte exploredWalls[5][9] = {0};
+  String statusCells[5][9];
+  byte rows = 5;
+  byte cols = 9;
+  byte startX = 0;
+  byte startY = 0;
+  Head head;
+
+public:
+  Maze(String const &h)
+  {
+    initializeCells();         // 2 is unexplored
+    initializeStatusCells();   // 4 spaces
+    statusCells[2][4] = " X "; // mark goal
+  }
+  void initializeCells()
+  {
+    for (byte i = 0; i < 5; i++)
+    {
+      for (byte j = 0; j < 9; j++)
+      {
+        if (i == 0)
+        {
+          cells[i][j] |= (1 << 3);
+        }
+        if (i == 4)
+        {
+          cells[i][j] |= (1 << 1);
+        }
+        if (j == 0)
+        {
+          cells[i][j] |= (1 << 0);
+        }
+        if (j == 8)
+        {
+          cells[i][j] |= (1 << 2);
+        }
+      }
     }
-    void initializeCells() {
-      for (byte i = 0; i < 5 ; i++) {
-        for (byte j = 0; j < 9; j++) {
-          for (byte k = 0; k < 4; k++) {
-            cells[i][j][k] = 2; // 2 is unexplored
-            if (i == 0) {
-              cells[i][j][0] = 1;
-            }
-            if (i == 4) {
-              cells[i][j][2] = 1;
-            }
-            if (j == 0) {
-              cells[i][j][3] = 1;
-            }
-            if (j == 8) {
-              cells[i][j][1] = 1;
-            }
+  }
+  void initializeStatusCells()
+  {
+    for (byte i = 0; i < 5; i++)
+    {
+      for (byte j = 0; j < 9; j++)
+      {
+        // set every cells to 4 spaces
+        statusCells[i][j] = "   ";
+      }
+    }
+  }
+  void updateStatusCells(byte i, byte j, String const &h)
+  {
+    // to update these cells with path
+    // data and start/end
+    if (h == "E")
+    {
+      head = EAST;
+    }
+    else if (h == "S")
+    {
+      head = SOUTH;
+    }
+    else if (h == "W")
+    {
+      head = WEST;
+    }
+    else if (h == "N")
+    {
+      head = NORTH;
+    }
+    statusCells[i][j] = String(" " + h + " ");
+    startX = i;
+    startY = j;
+    //      Serial.print("Updated");
+    //      Serial.print(i); Serial.print(j); Serial.println(statusCells[i][j]);
+  }
+  void addPath(byte i, byte j, byte val)
+  {
+    Serial.println(val);
+    String space = " ";
+    String str = String(val);
+    space.concat(str);
+    if (val < 10)
+    {
+      space.concat(" ");
+    }
+    statusCells[i][j] = space;
+  }
+  void fillCells(String const &h)
+  {
+    byte index = 0;
+    byte len = h.length();
+    while (index < len)
+    {
+      byte i = h.charAt(index) - '0';
+      index++;
+      byte j = h.charAt(index) - '0';
+      index++;
+      char a = h.charAt(index);
+      byte data = x2i(&a);
+      index++;
+      // Now update the cell data
+      // byte data = decimal * 10 + digit;
+      // BITMASK E-S-W-N
+      cells[i][j] |= (data & B1000); // EAST
+      cells[i][j] |= (data & B0100); // SOUTH
+      cells[i][j] |= (data & B0010); // WEST
+      cells[i][j] |= (data & B0001); // NORTH
+      // Also need to mark cells as explored
+      exploredWalls[i][j] = 0x0F;
+      updateNeighbours(i, j);
+    }
+  }
+  void updateNeighbours(byte i, byte j)
+  {
+    if (i >= 0 && i < 4)
+    {
+      // EAST <--> WEST
+      cells[i + 1][j] |= ((cells[i][j] & (1 << 1)) << 2);
+      exploredWalls[i + 1][j] |= (1 << 3);
+      //cells[i + 1][j][0] = cells[i][j][2];
+    }
+    if (i <= 4 && i > 0)
+    {
+      // WEST <--> EAST
+      cells[i - 1][j] |= ((cells[i][j] & (1 << 3)) >> 2);
+      exploredWalls[i - 1][j] |= (1 << 1);
+      //cells[i - 1][j][2] = cells[i][j][0];
+    }
+    if (j >= 0 && j < 8)
+    {
+      // NORTH <--> SOUTH
+      cells[i][j + 1] |= ((cells[i][j] & (1 << 2)) >> 2);
+      exploredWalls[i][j + 1] |= (1 << 0);
+      //cells[i][j + 1][3] = cells[i][j][1];
+    }
+    if (j <= 8 && j > 0)
+    {
+      // SOUTH <--> NORTH
+      cells[i][j - 1] |= ((cells[i][j] & (1 << 0)) << 2);
+      exploredWalls[i][j - 1] |= (1 << 2);
+      //cells[i][j - 1][1] = cells[i][j][3];
+    }
+    if (i > 0 && i < 4 && j > 0 && j < 8)
+    {
+      cells[i + 1][j] |= ((cells[i][j] & (1 << 1)) << 2);
+      cells[i - 1][j] |= ((cells[i][j] & (1 << 3)) >> 2);
+      cells[i][j + 1] |= ((cells[i][j] & (1 << 2)) >> 2);
+      cells[i][j - 1] |= ((cells[i][j] & (1 << 0)) << 2);
+      exploredWalls[i + 1][j] |= (1 << 3);
+      exploredWalls[i - 1][j] |= (1 << 1);
+      exploredWalls[i][j + 1] |= (1 << 0);
+      exploredWalls[i][j - 1] |= (1 << 2);
+      // cells[i + 1][j][0] = cells[i][j][2];
+      // cells[i - 1][j][2] = cells[i][j][0];
+      // cells[i][j + 1][3] = cells[i][j][1];
+      // cells[i][j - 1][1] = cells[i][j][3];
+    }
+  }
+  byte getCellData(byte i, byte j, byte k)
+  {
+    return cells[i][j][k];
+  }
+  byte getStartX()
+  {
+    return startX;
+  }
+  byte getStartY()
+  {
+    return startY;
+  }
+  void print()
+  {
+    for (byte j = 0; j < cols; j++)
+    {
+      Serial.print(F(" ---")); // Print top walls
+    }
+    Serial.print("\n");
+    for (byte i = 0; i < rows; i++)
+    {
+      // Print Vertical;
+      Serial.print("|"); // left most wall
+      for (byte j = 0; j < cols - 1; j++)
+      {
+        Serial.print(statusCells[i][j]);
+        // if not explored
+        if (!(exploredWalls[i][j] & (1 << 2))
+        {
+          Serial.print("*");
+        }
+        else
+        {
+          if (cells[i][j][1] == 1)
+          {
+            Serial.print("|");
+          }
+          else
+          {
+            Serial.print(" ");
           }
         }
       }
-    }
-    void initializeStatusCells() {
-      for (byte i = 0; i < 5 ; i++) {
-        for (byte j = 0; j < 9; j++) {
-          // set every cells to 4 spaces
-          statusCells[i][j] = "   ";
+      Serial.print(statusCells[i][cols - 1]);
+      Serial.print("|"); // right most wall
+      Serial.print("\n");
+      if (i == 4)
+      {
+        break;
+      }
+      for (byte j = 0; j < cols; j++)
+      {
+        if (cells[i][j][2] == 2)
+        {
+          Serial.print(F(" ***"));
         }
-      }
-    }
-    void updateStatusCells(byte i, byte j, String const&h) {
-      // to update these cells with path
-      // data and start/end
-      if (h == "E") {
-        head = EAST;
-      } else if (h == "S") {
-        head = SOUTH;
-      } else if (h == "W") {
-        head = WEST;
-      } else if (h == "N") {
-        head = NORTH;
-      }
-      statusCells[i][j] = String(" " + h + " ");
-      startX = i;
-      startY = j;
-      //      Serial.print("Updated");
-      //      Serial.print(i); Serial.print(j); Serial.println(statusCells[i][j]);
-    }
-    void addPath(byte i, byte j, byte val) {
-      Serial.println(val);
-      String space = " ";
-      String str = String(val);
-      space.concat(str);
-      if (val < 10) {
-        space.concat(" ");
-      }
-      statusCells[i][j] = space;
-    }
-    void fillCells(String const& h) {
-      byte index = 0;
-      byte len = h.length();
-      while (index < len) {
-        byte i = h.charAt(index) - '0';
-        index++;
-        byte j = h.charAt(index) - '0';
-        index++;
-        char a = h.charAt(index);
-        byte data = x2i(&a);
-        index++;
-        // Now update the cell data
-        // byte data = decimal * 10 + digit;
-        // BITMASK E-S-W-N
-        cells[i][j][0] = (data & B1000) >> 3;
-        cells[i][j][1] = (data & B0100) >> 2;
-        cells[i][j][2] = (data & B0010) >> 1;
-        cells[i][j][3] = (data & B0001) >> 0;
-        updateAdjCells(i, j, data);
-      }
-    }
-    void updateAdjCells(byte i, byte j, byte data) {
-      if ( i >= 0 && i < 4 ) {
-        // can go down
-        cells[i + 1][j][0] = cells[i][j][2];
-      }
-      if ( i <= 4 && i > 0 ) {
-        // can go up
-        cells[i - 1][j][2] = cells[i][j][0];
-      }
-      if ( j >= 0 && j < 8 ) {
-        cells[i][j + 1][3] =  cells[i][j][1];
-      }
-      if ( j <= 8 && j > 0 ) {
-        cells[i][j - 1][1] =  cells[i][j][3];
-      }
-      if ( i > 0 && i < 4 && j > 0 && j < 8 ) {
-        cells[i + 1][j][0] = cells[i][j][2];
-        cells[i - 1][j][2] = cells[i][j][0];
-        cells[i][j + 1][3] =  cells[i][j][1];
-        cells[i][j - 1][1] =  cells[i][j][3];
-      }
-    }
-    byte getCellData(byte i, byte j, byte k) {
-      return cells[i][j][k];
-    }
-    byte getStartX() {
-      return startX;
-    }
-    byte getStartY() {
-      return startY;
-    }
-    void print() {
-      for (byte j = 0; j < cols; j++) {
-        Serial.print(F(" ---")); // Print top walls
+        else
+        {
+          if (cells[i][j][2] == 1)
+          {
+            Serial.print(F(" ---"));
+          }
+          else
+          {
+            Serial.print(F("    "));
+          }
+        }
       }
       Serial.print("\n");
-      for (byte i = 0; i < rows; i++) {
-        // Print Vertical;
-        Serial.print("|"); // left most wall
-        for (byte j = 0; j < cols - 1; j++) {
-          Serial.print(statusCells[i][j]);
-          if (cells[i][j][1] == 2) {
-            Serial.print("*");
-          } else {
-            if (cells[i][j][1] == 1) {
-              Serial.print("|");
-            } else {
-              Serial.print(" ");
-            }
-          }
-        }
-        Serial.print(statusCells[i][cols - 1]);
-        Serial.print("|"); // right most wall
-        Serial.print("\n");
-        if (i == 4) {
-          break;
-        }
-        for (byte j = 0; j < cols; j++) {
-          if (cells[i][j][2] == 2) {
-            Serial.print(F(" ***"));
-          } else {
-            if (cells[i][j][2] == 1) {
-              Serial.print(F(" ---"));
-            } else {
-              Serial.print(F("    "));
-            }
-          }
-        }
-        Serial.print("\n");
-      }
-      for (byte j = 0; j < cols; j++) {
-        Serial.print(F(" ---")); // Print closing bottom walls
-      }
-      Serial.print("\n");
     }
-    Head getHeading() {
-      return head;
+    for (byte j = 0; j < cols; j++)
+    {
+      Serial.print(F(" ---")); // Print closing bottom walls
     }
+    Serial.print("\n");
+  }
+  Head getHeading()
+  {
+    return head;
+  }
 };
 #endif
--- PhaseB/newmaze3d/Path.h
+++ PhaseB/newmaze3d/Path.h
@@ -3,62 +3,77 @@
 #include "LinkedList.h"
 #include "Maze.h"
 #include <Arduino.h>
-class Node {
-  private:
-    byte x;
-    byte y;
-    byte v;  //value
-    byte t;
-    Head from;
-  public:
-    Node(byte x, byte y, byte v, byte t, byte k) {
-      this->x = x;
-      this->y = y;
-      this->v = v;
-      this->t = t;
-      this->from = (Head)k;
-    }
-    byte getX() {
-      return x;
-    }
-    byte getY() {
-      return y;
-    }
-    byte getValue() {
-      return v;
-    }
-    byte getTurn() {
-      return t;
-    }
-    Head getHead() {
-      return from;
-    }
-    void print() {
-      Serial.print("X ");
+#include <avr/pgmspace.h>
+class Node
+{
+private:
+  byte x;
+  byte y;
+  byte v; //value
+  byte t;
+  Head from;
+
+public:
+  Node(byte x, byte y, byte v, byte t, byte k)
+  {
+    this->x = x;
+    this->y = y;
+    this->v = v;
+    this->t = t;
+    this->from = (Head)k;
+  }
+  byte getX()
+  {
+    return x;
+  }
+  byte getY()
+  {
+    return y;
+  }
+  byte getValue()
+  {
+    return v;
+  }
+  byte getTurn()
+  {
+    return t;
+  }
+  Head getHead()
+  {
+    return from;
+  }
+  void print()
+  {
+      Serial.print(F(("X "));
       Serial.print(x);
-      Serial.print(" Y ");
+      Serial.print(F((" Y "));
       Serial.print(y);
-      Serial.print(" Val ");
+      Serial.print(F(" Val "));
       Serial.print(v);
-      Serial.print(" Head : ");
+      Serial.print(F(" Head : "));
       Serial.println(from);
-    }
+  }
 };
 
-class Path {
-  public:
-    byte turn = 0;
-    LinkedList<Node*> path;
-    Path() {
-      path = LinkedList<Node*>();
+class Path
+{
+public:
+  byte turn = 0;
+  LinkedList<Node *> path;
+  Path()
+  {
+    path = LinkedList<Node *>();
+  }
+  void add(Node *n)
+  {
+    path.add(n);
+  }
+  void print()
+  {
+    for (byte i = 0; i < path.size(); i++)
+    {
+      path.get(i)->print();
     }
-    void add(Node * n) {
-      path.add(n);
-    }
-    void print() {
-      for (byte i = 0; i < path.size(); i++) {
-        path.get(i)->print();
-      }
-    }
+  }
 };
 #endif
