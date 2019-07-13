988e51cd97d0f714ef2265d4ea9ae0f9f28c5afe
Kanadech Jirapongtanavech <kanadech.king@gmail.com>
2019-07-14 01:18:55 +1000

    Used array instead of list

--- PhaseB/newmaze3d/newmaze3d.ino
+++ PhaseB/newmaze3d/newmaze3d.ino
@@ -6,8 +6,11 @@
 
 Maze *maze = new Maze("");
 Floodfill *flood = new Floodfill(maze);
-LinkedList<Node*> path = LinkedList<Node*>();
-LinkedList<char> *commands = new LinkedList<char>();
+LinkedList<Node *> path = LinkedList<Node *>();
+//LinkedList<char> *commands = new LinkedList<char>();
+char commands[40]; // for storing commands
+byte commandCount = 0;
+
 void setup()
 {
   Serial.begin(9600);
@@ -16,106 +19,101 @@ void setup()
 
 void loop()
 {
-  if (Serial.available()) {
+  if (Serial.available())
+  {
     String c = Serial.readString();
     Serial.println(c);
-    if (c.startsWith("f")) {
+    if (c.startsWith("f"))
+    {
       flood->AssumeNoWalls();
       flood->doFloodfill();
       flood->print();
       flood->AssumeWalls();
       flood->doFloodfill();
       flood->print();
-      Serial.print(F("SufficientlyExplored? "));
+      Serial.print(F("Is maze sufficiently explored? "));
       Serial.println(flood->sufficientlyExplored());
     }
-    else if (c.startsWith("p")) {
+    else if (c.startsWith("p"))
+    {
       flood->printCell(c.charAt(1) - '0', c.charAt(2) - '0');
     }
-    else if (c.startsWith("s")) {
+    else if (c.startsWith("s"))
+    {
       Serial.println(c.substring(3));
       maze->updateStatusCells(c.charAt(1) - '0', c.charAt(2) - '0', c.substring(3));
       maze->print();
     }
-    else if (c.startsWith("m")) {
+    else if (c.startsWith("m"))
+    {
       createPath();
-      for (byte i = 0; i < path.size(); i++) {
+      for (byte i = 0; i < path.size(); i++)
+      {
         path.get(i)->print();
         byte num = path.get(i)->getValue();
         num = abs(flood->getCell(maze->getStartX(), maze->getStartY()) - num);
         // Add to maze for printing
         maze->addPath(path.get(i)->getX(), path.get(i)->getY(), num);
       }
-      // first add forward
-      for (byte i = 0; i < path.size(); i++) {
-        commands->add('F');
-        if (i == path.size() - 1) {
-          continue;
-        }
-        if (path.get(i)->getHead() == 0 && path.get(i + 1)->getHead() == 3){
-          commands->add('L'); // left
-        } else if (path.get(i)->getHead() == 0 && path.get(i + 1)->getHead() == 1){
-          commands->add('R'); // right 
-        } else if (path.get(i)->getHead() == 3 && path.get(i + 1)->getHead() == 0){
-          commands->add('R'); // right 
-        } else if (path.get(i)->getHead() == 3 && path.get(i + 1)->getHead() == 2){
-          commands->add('L'); // left
-        } else if (path.get(i)->getHead() > path.get(i + 1)->getHead()) {
-          commands->add('L'); // left
-        } else if (path.get(i)->getHead() < path.get(i + 1)->getHead()) {
-          commands->add('R'); // right
-        }
-      }
+      // first add forward then turn
+      // We need to reset commandCount before doing so
+      resetCommand();
+      fillCommandArray();
       maze->print();
-      Serial.println("Commands");
-      for (byte i = 0; i < commands->size(); i++) {
-        Serial.print(commands->get(i));
-        Serial.print("  ");
-      }
-      Serial.println("");
+      printCommand();
     }
-    else {
+    else
+    {
       maze->fillCells(c);
       maze->print();
     }
   }
 }
 
-void createPath() {
+void createPath()
+{
   //Clear path
   clearList(&path);
-  Head h = maze->getHeading();   // starting heading
-  Node * currN = nullptr;
-  Node * toDelete = nullptr;
-  LinkedList<Node *> stack = LinkedList<Node *>();	// Have a stack to keep all possible nodes we can traverse
+  Head h = maze->getHeading(); // starting heading
+  Node *currN = nullptr;
+  Node *toDelete = nullptr;
+  LinkedList<Node *> stack = LinkedList<Node *>(); // Have a stack to keep all possible nodes we can traverse
   // move towards the neighbouring cells that has less than 1
   byte currX = maze->getStartX();
   byte currY = maze->getStartY();
   byte currV = flood->getCell(currX, currY);
   byte movement = 1;
-  while (movement != 0) {
+  while (movement != 0)
+  {
     movement = 0;
     // for all cells neighbouring curr cells
     // add valid cells to stack
-    for (byte k = 0; k < 4; k++) {
-      if (maze->getCellData(currX, currY, k) == 0) {
+    for (byte k = 0; k < 4; k++)
+    {
+      if (maze->getCellData(currX, currY, k) == 0)
+      {
         // No wall here. Find out the cell Pose
         byte tmpX = currX;
         byte tmpY = currY;
-        if (k == 0) {
+        if (k == 0)
+        {
           tmpX--;
         }
-        if (k == 1) {
+        if (k == 1)
+        {
           tmpY++;
         }
-        if (k == 2) {
+        if (k == 2)
+        {
           tmpX++;
         }
-        if (k == 3) {
+        if (k == 3)
+        {
           tmpY--;
         }
         byte nextV = flood->getCell(tmpX, tmpY);
-        if ((currV - nextV) == 1) {
+        if ((currV - nextV) == 1)
+        {
           // We want CurrV - nextCell = 1
           // this is the right cell add to stack
           stack.add(new Node(tmpX, tmpY, nextV, abs(h - k) % 2, k));
@@ -126,11 +124,14 @@ void createPath() {
     // Among these cells we pick the one that does not require turning
     // (If possible)
     // And clear the stack
-    if (stack.size() != 0) {
+    if (stack.size() != 0)
+    {
       byte minTurn = 100;
       byte minTurnIndex = 0;
-      for (byte i = 0; i < stack.size(); i++) {
-        if (minTurn > stack.get(i)->getTurn()) {
+      for (byte i = 0; i < stack.size(); i++)
+      {
+        if (minTurn > stack.get(i)->getTurn())
+        {
           minTurn = stack.get(i)->getTurn();
           minTurnIndex = i;
         }
@@ -145,12 +146,14 @@ void createPath() {
       currV = currN->getValue();
       h = currN->getHead();
       // Free all elems in stack
-      while (stack.size() > 0) {
+      while (stack.size() > 0)
+      {
         toDelete = stack.pop();
         delete toDelete;
       }
       movement = 1;
-      if (currV == 0) {
+      if (currV == 0)
+      {
         break;
       }
     }
@@ -158,13 +161,81 @@ void createPath() {
   clearList(&stack);
 }
 
-void clearList(LinkedList<Node*> *list) {
-  while (list->size() > 0) {
-    Node * toDelete = list->pop();
+void clearList(LinkedList<Node *> *list)
+{
+  while (list->size() > 0)
+  {
+    Node *toDelete = list->pop();
     delete toDelete;
   }
   list->clear();
 }
+
+/*
+Assuming a path exist, commands array can be filled by calling this function
+*/
+void fillCommandArray()
+{
+  for (byte i = 0; i < path.size(); i++)
+  {
+    addCommand(commandCount, 'F');
+    if (i == path.size() - 1)
+    {
+      continue;
+    }
+    if (path.get(i)->getHead() == 0 && path.get(i + 1)->getHead() == 3)
+    {
+      addCommand(commandCount, 'L'); // left
+    }
+    else if (path.get(i)->getHead() == 0 && path.get(i + 1)->getHead() == 1)
+    {
+      addCommand(commandCount, 'R'); // right
+    }
+    else if (path.get(i)->getHead() == 3 && path.get(i + 1)->getHead() == 0)
+    {
+      addCommand(commandCount, 'R'); // right
+    }
+    else if (path.get(i)->getHead() == 3 && path.get(i + 1)->getHead() == 2)
+    {
+      addCommand(commandCount, 'L'); // left
+    }
+    else if (path.get(i)->getHead() > path.get(i + 1)->getHead())
+    {
+      addCommand(commandCount, 'L'); // left
+    }
+    else if (path.get(i)->getHead() < path.get(i + 1)->getHead())
+    {
+      addCommand(commandCount, 'R'); // right
+    }
+  }
+}
+
+void addCommand(byte i, char c)
+{
+  commands[i] = c;
+  commandCount++;
+}
+
+void resetCommand()
+{
+  for (int i = 0; i < 50; i++)
+  {
+    commands[i] = ' ';
+  }
+  commandCount = 0;
+}
+
+void printCommand()
+{
+  Serial.println(F("Commands to destination"));
+  for (int i = 0; i < commandCount; i++)
+  {
+    Serial.print(commands[i]);
+    Serial.print("  ");
+  }
+  Serial.println("");
+}
+
 // ESWN e.g. All 4 walls is F in HEX 1111
 // Give Cell position 00F
 //   E
