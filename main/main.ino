944cef00ede1cc117ceb97ebe5d51e01d8a29594
Liangde LI <liliangde88@gmail.com>
2019-07-16 16:37:33 +1000

    Some more clean up and basic test

--- main/main.ino
+++ main/main.ino
@@ -1,84 +1,89 @@
+// Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
+// is used in I2Cdev.h
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    #include "Wire.h"
+#endif
 #include "Sensors.h"
 #include "locomotion.h"
 using namespace hardware;
 using namespace hardware::pins;
-//#include "MPU6050.h" // not necessary if using MotionApps include file
-
-// Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
-// is used in I2Cdev.h
-#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-#include "Wire.h"
-#endif
 
 float Yaw;
-long current, timeIntervel;
-char keyword;
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(19200);
-  while (! Serial);
-  Serial.println("Serial setup finish!");
+    // put your setup code here, to run once:
+    Serial.begin(19200);
+    while (! Serial);
+    Serial.println("Serial setup finish!");
 
-  // setup leds
-  leds_setup();
-  Serial.println("Leds setup finish!");
+    // setup leds
+    leds_setup();
+    Serial.println("Leds setup finish!");
 
-  // setup locomotion
-  locomotion_setup();
-  Serial.println("Locomotion setup finish!");
+    // setup locomotion
+    locomotion_setup();
+    Serial.println("Locomotion setup finish!");
 
-  //setup Sensors
-  sensorSetup();
-  // setup map
+    //setup Sensors
+    sensorSetup();
+    // setup map
 
-  // Wait 5 seconds and main loop start
-  delay(5000);
+    // Wait 5 seconds and main loop start
+    delay(5000);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  double distance_f, distance_l, distance_r;
-  //Array be used to detect whether there is wall on the front of sensors, left, front and right respectively, 1 have wall 0 otherwise
-  int lfr[3];
-  //Variable that indicate the car to move front(0), left(1), right(2), back(3)
-  int move_dir = 0;
-  //Varianle that indicate which direction (East(1), South(2), West(3),North(4)) the car is facing
-  int face_dir = 0;
-  //Array be used to determine whether there is al wall at East, South, West and North
-  int ESWN[4] = {0, 0, 0, 0};
+    // put your main code here, to run repeatedly:
+    double distance_f, distance_l, distance_r;
+    //Array be used to detect whether there is wall on the front of sensors, left, front and right respectively, 1 have wall 0 otherwise
+    int lfr[3];
+    //Variable that indicate the car to move front(0), left(1), right(2), back(3)
+    int move_dir = 0;
+    //Varianle that indicate which direction (East(1), South(2), West(3),North(4)) the car is facing
+    int face_dir = 0;
+    //Array be used to determine whether there is al wall at East, South, West and North
+    int ESWN[4] = {0, 0, 0, 0};
 
 
-  // 0. Read from bluetooth? Any command?
+    // 0. Read from bluetooth? Any command?
 
 
-  // 1. perception
+    // 1. perception
 
 
-  // 1.1.Read from wheel
+    // 1.1.Read from wheel
 
 
-  // 1.2.Read from sensors
-  //function that dectect the left, front and right wall near the car
-  //front distance (ultrasonic distance)
-  distance_f = sonar<pins::sonar_trigger, pins::sonar_echo>::distance().count();        //convert the CM to MM
-  //left distance (left lidar)
-  distance_l = lidar<lidar_tag<0>>::distance().count();
-  //right distance (right lidar)
-  distance_r = lidar<lidar_tag<1>>::distance().count();
-  carWall(distance_l, distance_f, distance_r, lfr);
-  //update IMU parameters
-  imu::update();
-  //Get IMU yaw value
-  Yaw = imu::yaw();
-  //function that records the walls near car (Ease, South, West, North)
-  ESWNWall(Yaw, lfr, ESWN, &face_dir);
+    // 1.2.Read from sensors
+    //function that dectect the left, front and right wall near the car
+    //front distance (ultrasonic distance)
+    distance_f = sonar<pins::sonar_trigger, pins::sonar_echo>::distance().count();        //convert the CM to MM
+    //left distance (left lidar)
+    distance_l = lidar<lidar_tag<0>>::distance().count();
+    //right distance (right lidar)
+    distance_r = lidar<lidar_tag<1>>::distance().count();
+    carWall(distance_l, distance_f, distance_r, lfr);
+    //update IMU parameters
+    imu::update();
+    //Get IMU yaw value
+    Yaw = imu::yaw();
+    //function that records the walls near car (Ease, South, West, North)
+    ESWNWall(Yaw, lfr, ESWN, &face_dir);
 
 
-  // 2. Localisation and planning
+    // 2. Localisation and planning
 
 
-  // 3. Give command to locomotion
+    // 3. Give command to locomotion
 
 
+    // Test code
+    Serial.print("Front: ");
+    Serial.print(distance_f);
+    Serial.print(" Left: ");
+    Serial.print(distance_l);
+    Serial.print(" Right: ");
+    Serial.print(distance_r);
+    Serial.print(" Yaw: ");
+    Serial.println(Yaw);
 }
