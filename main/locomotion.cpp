f6eab34c5857b5f3acb451777e36974f9d4df6d4
Liangde LI <liliangde88@gmail.com>
2019-07-19 10:56:47 +1000

    some work

--- main/locomotion.cpp
+++ main/locomotion.cpp
@@ -6,7 +6,11 @@
  *
  * @note 
  * */
+#include <cstdlib>
 #include "locomotion.h"
+
+#define THRESHOLD_LEFT  40
+#define THRESHOLD_RIGHT 40
 
 extern int counterA;
 extern int counterB;
@@ -15,8 +19,13 @@ extern int motion_mode;
 int goalA=0;
 int goalB=0;
 
-void callback1(void);
-void callback2(void);
+static double vLeft=0;
+static double vRight=0;
+
+static void apply_speed(void);
+static void motor_controller(double distance_f, double distance_l, double distance_r);
+static void callback1(void);
+static void callback2(void);
 
 void leds_setup(void)
 {
@@ -48,9 +57,9 @@ void turning(void)
         counterA=0;
         goalB=68;
         counterB=0;
-        units::percentage test_speed(50);
-        hardware::left_motor::backward(test_speed);
-        hardware::right_motor::forward(test_speed);
+        vLeft=-50.0;
+        vRight=50.0;
+        apply_speed();
     }
     else if (motion_mode == MOTION_RIGHT)
     {
@@ -58,9 +67,9 @@ void turning(void)
         counterA=0;
         goalB=-68;
         counterB=0;
-        units::percentage test_speed(50);
-        hardware::left_motor::forward(test_speed);
-        hardware::right_motor::backward(test_speed);
+        vLeft=50.0;
+        vRight=-50.0;
+        apply_speed();
     }
     else if (motion_mode == MOTION_BACK)
     {
@@ -68,13 +77,72 @@ void turning(void)
         counterA=0;
         goalB=141;
         counterB=0;
-        units::percentage test_speed(50);
-        hardware::left_motor::backward(test_speed);
-        hardware::right_motor::forward(test_speed);
+        vLeft=-50.0;
+        vRight=50.0;
+        apply_speed();
     }
     else
     {
         Serial.println("Warning: Motion mode not properly setup in turning!");
+    }
+}
+
+void forward(int ncells, double distance_f, double distance_l, double distance_r)
+{
+    goalA = ncells*250;
+    counterA = 0;
+    goalB = ncells*250;
+    counterB = 0;
+    vLeft=50.0;
+    vRight=50.0;
+    apply_speed();
+}
+
+void forward_updating(double distance_f, double distance_l, double distance_r)
+{
+
+}
+
+/**
+ * @brief Send the distance data to locomotion module
+ *
+ * @param 
+ *
+ * @return 
+ *
+ * @note Send the distance data to locomotion module
+ * */
+void apply_speed(void)
+{
+    units::percentage left_speed(std::abs(vLeft));
+    units::percentage right_speed(std::abs(vRight));
+    
+    if (vLeft >= 0)
+        hardware::left_motor::forward(left_speed);
+    else
+        hardware::left_motor::backward(left_speed);
+    
+    if (vRight >= 0)
+        hardware::right_motor::forward(right_speed);
+    else
+        hardware::right_motor::backward(right_speed);
+}
+
+/**
+ * @brief Send the distance data to locomotion module
+ *
+ * @param 
+ *
+ * @return 
+ *
+ * @note Send the distance data to locomotion module
+ * */
+void motor_controller(double distance_f, double distance_l, double distance_r)
+{
+    // Goto mild controller depending on counters
+    if( (distance_l < distance_l > THRESHOLD_LEFT && distance_r > THRESHOLD_RIGHT))
+    {
+
     }
 }
 
@@ -100,22 +168,6 @@ void callback1(void)
         goalA=0;
         if (goalB==0) motion_mode = MOTION_STOP;
     }
-}
-
-void forward(int ncells, double distance_f, double distance_l, double distance_r)
-{
-    goalA=250;
-    counterA=0;
-    goalB=250;
-    counterB=0;
-    units::percentage test_speed(50);
-    hardware::left_motor::forward(test_speed);
-    hardware::right_motor::forward(test_speed);
-}
-
-void forward_updating(double distance_f, double distance_l, double distance_r)
-{
-    
 }
 
 void callback2(void)
